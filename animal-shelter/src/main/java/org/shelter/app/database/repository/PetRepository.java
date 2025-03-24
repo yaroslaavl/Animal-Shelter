8be9bd81a50760ddddd966eb71e166872e0cceae
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-03-24 01:38:31 +0100

    24.03.2025 - feature/adoption-logic | custom methods

--- animal-shelter/src/main/java/org/shelter/app/database/repository/PetRepository.java
+++ animal-shelter/src/main/java/org/shelter/app/database/repository/PetRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface PetRepository extends JpaRepository<Pet,Long> {
+public interface PetRepository extends JpaRepository<Pet,Long>, JpaSpecificationExecutor<Pet> {
 
     @Query("SELECT p FROM Pet p " +
             "JOIN FETCH p.species WHERE p.id = :id")
