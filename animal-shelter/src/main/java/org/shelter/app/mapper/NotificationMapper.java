03ae1b39c28d7ec6fa81dfa2a1ddecd81813f2a3
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-03-24 01:38:04 +0100

    24.03.2025 - feature/adoption-logic | added toEntity method in mapper

--- animal-shelter/src/main/java/org/shelter/app/mapper/NotificationMapper.java
+++ animal-shelter/src/main/java/org/shelter/app/mapper/NotificationMapper.java
@@ -3,6 +3,7 @@ package org.shelter.app.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.shelter.app.database.entity.Notification;
+import org.shelter.app.dto.NotificationCreateDto;
 import org.shelter.app.dto.NotificationReadDto;
 
 @Mapper(componentModel = "spring")
@@ -11,4 +12,11 @@ public interface NotificationMapper {
     @Mapping(source = "user.id", target = "userId")
     @Mapping(source = "adoptionRequest.id", target = "adoptionRequestId")
     NotificationReadDto toDto(Notification notification);
+
+    @Mapping(target = "isRead", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "userId", target = "user.id")
+    @Mapping(source = "adoptionRequestId", target = "adoptionRequest.id")
+    Notification toEntity(NotificationCreateDto notificationCreateEditDto);
 }
